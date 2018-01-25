170af0d55572a6d37f77204cd155520d571f724b
Aaron Smith <projects@aaronsmith.co>
2018-01-24 17:57:26 -0800

    adding install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+apt-get install software-properties-common
+add-apt-repository ppa:neovim-ppa/stable
+apt-get update
+
+apt-get install python-dev python-pip python3-dev python3-pip
+
+apt-get install neovim git silversearcher-ag exuberant-ctags
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+
+git clone https://github.com/crypticsymbols/.vim
+
+mkdir ~/.config
+
+ln -s ~/.vim ~/.config/nvim
+
+pip3 install --user neovim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+nvim +PluginInstall +qall
